af8561a732b704cb153904eefb9cc5cc7b5dd19f
Anze Demsar <demshy@gmail.com>
2022-07-05 20:58:35 +0200

    fix deploy yaml

--- config/server.js
+++ config/server.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  url: 'https://plezanje.net/content',
 });
