42e9b8149a84a3228c4748ab87baa023f1e0e16d
Anze Demsar <demshy@gmail.com>
2022-07-05 22:41:50 +0200

    add app keys to server

--- config/server.js
+++ config/server.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'https://plezanje.net/content',
+  app: {
+    keys: env.array('APP_KEYS')
+  }
 });
